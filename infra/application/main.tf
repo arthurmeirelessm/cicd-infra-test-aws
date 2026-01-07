a756841b7c4014a29c70c858dd4d47867e5ce847
Arthur Meireles <arthurmsesjdfdn@gmail.com>
2026-01-07 03:11:24 -0300

    change path codebuld infra / v6

--- infra/application/main.tf
+++ infra/application/main.tf
@@ -48,3 +48,14 @@ module "hello_lambda" {
   role_arn        = module.iam_lambda.role_arn
   placeholder_zip = "${path.module}/../placeholder.zip"
 }
+
+
+module "second_test_lambda" {
+  source = "../modules/lambda"
+
+  function_name   = "second-test-lambda"
+  runtime         = "python3.11"
+  handler         = "handler.handler"
+  role_arn        = module.iam_lambda.role_arn
+  placeholder_zip = "${path.module}/../placeholder.zip"
+}
